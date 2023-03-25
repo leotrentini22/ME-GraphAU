daafe4db41f6cad41e29e893b99173587818bee8
leotrentini22 <trentini.leonardo00@gmail.com>
2023-03-25 16:20:44 +0100

    dataset loader

--- OpenGraphAU/tool/dataset_process.py
+++ OpenGraphAU/tool/dataset_process.py
@@ -2,17 +2,14 @@ import os
 import pandas as pd
 import numpy as np
 # #
-# AUs = ['1', '2' ,'4', '5', '6', '7', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '22' ,'23', '24', '25', '26', '27', '32', '38', '39']
+ AUs = ['1', '2' ,'4', '6', '7', '10', '12', '15', '23', '24', '25', '26']
 # mcro_AUs = ['L1', 'R1', 'L2', 'R2', 'L4', 'R4', 'L6', 'R6', 'L10', 'R10', 'L12', 'R12', 'L14', 'R14']
-# total_AUs = AUs+mcro_AUs
+ total_AUs = AUs
 
-# new_dataset_train_img_list = []
-# new_dataset_val_img_list = []
-# new_dataset_test_img_list = []
+new_dataset_train_img_list = []
+new_dataset_val_img_list = []
+new_dataset_test_img_list = []
 
-# new_dataset_train_label_list = []
-# new_dataset_val_label_list = []
-# new_dataset_test_label_list = []
 
 
 # #BP4d
@@ -745,6 +742,7 @@ au_ids  = ['1', '2' ,'4', '6', '7', '10', '12', '15', '23', '24', '25', '26']
 
 
 list_path_prefix = '/home/trentini/ME-GraphAU/OpenGraphAU/data/AffWild2/list/' #'/work/vita/datasets/Aff-Wild2/Third_ABAW_Annotations/AU_Detection_Challenge/' #'Datasets/hybrid_dataset/AffWild2/list'
+img_path_vita = '/work/vita/datasets/Aff-Wild2/cropped_aligned/'
 
 #data/AffWild2/list
 #/work/vita/datasets/Aff-Wild2/Third_ABAW_Annotations/AU_Detection_Challenge/
@@ -862,7 +860,7 @@ new_dataset_val_label_list.append(AffWild2_val_image_label)
 
 au_labels = []
 au_img_path = []             # for loop not clear  ->   test_text is a number? Or it's just a loop over the folders
-for test_txt in test_list:   # test list = /work/vita/datasets/Aff-Wild2/Third_ABAW_Annotations/AU_Detection_Challenge/Validation_Set
+for test_txt in test_list:   # test list = directories in /work/vita/datasets/Aff-Wild2/Third_ABAW_Annotations/AU_Detection_Challenge/Validation_Set
     with open(os.path.join(os.path.join(label_root, test_path), test_txt), 'r') as f:
         lines = f.readlines()
     lines = lines[1:]
@@ -872,7 +870,7 @@ for test_txt in test_list:   # test list = /work/vita/datasets/Aff-Wild2/Third_A
         if -1 in line:
             continue
         au_labels.append(line.reshape(1, -1))
-        au_img_path.append(os.path.join(test_txt.split('.')[0], str(j+1).zfill(5)+'.jpg'))    # what does it do here?
+        au_img_path.append(os.path.join(os.path.join(img_path_vita,os.path.basename(os.path.normpath(test_txt.split('.')[0]))), str(j+1).zfill(5)+'.jpg'))    # what does it do here?
 
 
 au_labels = np.concatenate(au_labels, axis=0)
