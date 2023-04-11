e57c9b4c54e455ce85cd1de03c1c5b3e8ac6bb99
leotrentini22 <trentini.leonardo00@gmail.com>
2023-04-11 22:03:08 +0200

    bugs

--- OpenGraphAU/tool/calculate_AU_class_weights.py
+++ OpenGraphAU/tool/calculate_AU_class_weights.py
@@ -15,7 +15,11 @@ imgs_AUoccur = np.loadtxt(os.path.join(list_path_prefix, 'AffWild2_train_label.t
 AUoccur_rate = np.zeros((1, imgs_AUoccur.shape[1]))
 
 for i in range(imgs_AUoccur.shape[1]):
-    AUoccur_rate[0, i] = sum(imgs_AUoccur[:,i]>0) / float(sum(imgs_AUoccur[:,i]>=0))
+    if float(sum(imgs_AUoccur[:,i]>=0)) > 0:
+        AUoccur_rate[0, i] = sum(imgs_AUoccur[:,i]>0) / float(sum(imgs_AUoccur[:,i]>=0))
+    else:
+        AUoccur_rate[0, i] = 0.0000001
+
     # AUoccur_rate[0, i] = sum(imgs_AUoccur[:,i]>0) / float(imgs_AUoccur.shape[0])
 
 
