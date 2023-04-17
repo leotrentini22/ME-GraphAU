1da4a07c70fcbf69c15f2ff1a3cda424606cc472
leotrentini22 <trentini.leonardo00@gmail.com>
2023-04-17 20:27:51 +0200

    typos

--- OpenGraphAU/tool/calculate_AU_class_weights.py
+++ OpenGraphAU/tool/calculate_AU_class_weights.py
@@ -12,7 +12,7 @@ example of content in 'AffWild2_train_label_fold1.txt':
 
 
 imgs_AUoccur = np.loadtxt(os.path.join(list_path_prefix, 'AffWild2_train_label.txt'))
-imgs_AUoccur = imgs_AUoccur[:, [0,1,2,4,5,7,9,12,19,20,21,22]]
+#imgs_AUoccur = imgs_AUoccur[:, [0,1,2,4,5,7,9,12,19,20,21,22]]
 AUoccur_rate = np.zeros((1, imgs_AUoccur.shape[1]))
 
 for i in range(imgs_AUoccur.shape[1]):
@@ -20,7 +20,7 @@ for i in range(imgs_AUoccur.shape[1]):
 
 print(AUoccur_rate)
 
-AU_weight = 1.0 / AUoccur_rate
+AU_weight = np.where(AUoccur_rate!=0, np.divide(1.0,AUoccur_rate),0)
 AU_weight = AU_weight / AU_weight.sum() * AU_weight.shape[1]
 np.savetxt(os.path.join(list_path_prefix, 'AffWild2_train_weight.txt'), AU_weight, fmt='%f', delimiter='\t')
 # print(AU_weight.shape)
