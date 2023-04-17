573bc73b84a1058d9ca8af4686357ecf9f793c0d
leotrentini22 <trentini.leonardo00@gmail.com>
2023-04-17 11:55:40 +0200

    calculate au weights

--- OpenGraphAU/tool/calculate_AU_class_weights.py
+++ OpenGraphAU/tool/calculate_AU_class_weights.py
@@ -12,11 +12,13 @@ example of content in 'AffWild2_train_label_fold1.txt':
 
 
 imgs_AUoccur = np.loadtxt(os.path.join(list_path_prefix, 'AffWild2_train_label.txt'))
+imgs_AUoccur = imgs_AUoccur[:, [0,1,2,4,5,7,9,12,19,20,21,22]]
 AUoccur_rate = np.zeros((1, imgs_AUoccur.shape[1]))
 
 for i in range(imgs_AUoccur.shape[1]):
     AUoccur_rate[0, i] = sum(imgs_AUoccur[:,i]>0) / float(imgs_AUoccur.shape[0])
 
+print(AUoccur_rate)
 
 AU_weight = 1.0 / AUoccur_rate
 AU_weight = AU_weight / AU_weight.sum() * AU_weight.shape[1]
