4b0808cbe25851b12d2b60718c5c3bc229fc588e
leotrentini22 <trentini.leonardo00@gmail.com>
2023-04-19 19:27:31 +0200

    change checkpoints path

--- OpenGraphAU/tool/calculate_AU_class_weights.py
+++ OpenGraphAU/tool/calculate_AU_class_weights.py
@@ -18,9 +18,7 @@ AUoccur_rate = np.zeros((1, imgs_AUoccur.shape[1]))
 for i in range(imgs_AUoccur.shape[1]):
     AUoccur_rate[0, i] = sum(imgs_AUoccur[:,i]>0) / float(imgs_AUoccur.shape[0])
 
-print(AUoccur_rate)
-
-AU_weight = np.where(AUoccur_rate!=0, np.divide(1.0,AUoccur_rate),0)
+AU_weight = np.where(AUoccur_rate!=0, np.divide(1.0,AUoccur_rate), 0)
 AU_weight = AU_weight / AU_weight.sum() * AU_weight.shape[1]
 np.savetxt(os.path.join(list_path_prefix, 'AffWild2_train_weight.txt'), AU_weight, fmt='%f', delimiter='\t')
 # print(AU_weight.shape)
