97a6151732a3596fc9d9a495b491a1dbc3303d60
leotrentini22 <trentini.leonardo00@gmail.com>
2023-04-23 21:50:58 +0200

    bugs rafau

--- OpenGraphAU/dataset.py
+++ OpenGraphAU/dataset.py
@@ -5,12 +5,12 @@ from torch.utils.data import Dataset
 import os
 
 
-def make_dataset(image_list, label_list, au_relation=None, flag=0):
+def make_dataset(image_list, label_list, flag=0, au_relation=None):
     len_ = len(image_list)
     print(len(image_list))
     print(len(label_list))
     if flag>0.5:
-        len_=(len_)/2
+        len_=(len_2)/2  #this line here because in dataset_process it loaded twice the paths for rafau, so len(image_list)=2*len(label_list)
 
     if au_relation is not None:
         images = [(image_list[i].strip(),  label_list[i, :],au_relation[i,:]) for i in range(len_)]
@@ -140,7 +140,7 @@ class RAFAU(Dataset):
 
             # img labels
             eval_label_list_path = os.path.join(root_path, 'list', 'RAF_AU_test_label.txt')
-            eval_label_list = np.loadtxt(eval_label_list_path)  #  -> to change when we understand how to do it automatically
+            eval_label_list = np.loadtxt(eval_label_list_path)  
             flag=1
             self.data_list = make_dataset(eval_image_list, eval_label_list,flag)
 
