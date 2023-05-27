0f05f5c6d0e1870b17ee80bacfb99da963c53469
leotrentini22 <trentini.leonardo00@gmail.com>
2023-05-27 19:04:18 +0200

    minor changes

--- OpenGraphAU/tool/dataset_utils.py
+++ OpenGraphAU/tool/dataset_utils.py
@@ -11,30 +11,34 @@ def set_your_paths(name_dataset):
     #### RAFAU
     if name_dataset == 'rafau':
 
-        # list_path_prefix -> path of the folder where the list of paths of the images and the list of AUs will be stored
+        # list_path_prefix -> path of the folder where there will be stored files with the list of paths of the images and the list of AUs
         list_path_prefix = '/home/trentini/ME-GraphAU/OpenGraphAU/data/RAF-AU/list/'
+
 
         # PATH OF THE DATASET
         # this will depend on where you store the dataser
-        # img_path_vita -> folder in which are stored the images
-        # label_root -> folder in which are stored the labels (i.e. the AUs)
 
+        # img_path_vita -> folder in which are stored the images
         img_path_vita = '/work/vita/datasets/RAF-AU/aligned'
+
+        # label_root -> folder in which are stored the labels (i.e. the AUs)
         label_root = '/work/vita/datasets/RAF-AU/'
 
         return list_path_prefix, img_path_vita, label_root
     
     elif name_dataset == 'casme':
 
-        # list_path_prefix -> path of the folder where the list of paths of the images and the list of AUs will be stored
+        # list_path_prefix -> path of the folder where there will be stored files with the list of paths of the images and the list of AUs
         list_path_prefix = '/home/trentini/ME-GraphAU/OpenGraphAU/data/CASME2/list/'
+
 
         # PATH OF THE DATASET
         # this will depend on where you store the dataser
-        # img_path_vita -> folder in which are stored the images
-        # label_root -> file in which are stored the labels (i.e. the AUs) (here not a folder)
 
+        # img_path_vita -> folder in which are stored the images
         img_path_vita = '/work/vita/datasets/CASME2/Cropped/'
+
+        # label_root -> file in which are stored the labels (i.e. the AUs) (here not a folder)
         label_root = '/work/vita/datasets/CASME2/CASME2-coding-20140508.xlsx'
 
         # split in train, val and test
@@ -48,16 +52,17 @@ def set_your_paths(name_dataset):
     #### AFFWILD
     elif name_dataset == 'affwild':
 
-        # list_path_prefix -> path of the folder where the list of paths of the images and the list of AUs will be stored
+        # list_path_prefix -> path of the folder where there will be stored files with the list of paths of the images and the list of AUs
         list_path_prefix = '/home/trentini/face-skeleton-detection/data/AffWild2/list/' 
 
 
         # PATH OF THE DATASET
         # this will depend on where you store the dataser
-        # img_path_vita -> folder in which are stored the images
-        # label_root -> folder in which are stored the labels (i.e. the AUs)
 
+        # img_path_vita -> folder in which are stored the images
         img_path_vita = '/work/vita/datasets/Aff-Wild2/cropped_aligned/'
+
+        # label_root -> folder in which are stored the labels (i.e. the AUs)
         label_root = '/work/vita/datasets/Aff-Wild2/Third_ABAW_Annotations/AU_Detection_Challenge/'
 
 
