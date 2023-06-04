f7e6c04bd40c5e187888f94f77ac8426edddda09
leotrentini22 <trentini.leonardo00@gmail.com>
2023-06-04 14:53:08 +0200

    typos

--- OpenGraphAU/dataset.py
+++ OpenGraphAU/dataset.py
@@ -15,7 +15,7 @@ def make_dataset(image_list, label_list, flag=0, au_relation=None):
         len_=int((len_)/2)  #this line here because in dataset_process it loaded twice the paths for rafau, so len(image_list)=2*len(label_list)
 
     if au_relation is not None:
-        images = [(image_list[i].strip(),  label_list[i, :],au_relation[i,:]) for i in range(len_)]
+        images = [(image_list[i].strip(),  label_list[i, :],au_relation[i,:]) for i in range(445844)]
     else:
         images = [(image_list[i].strip(),  label_list[i, :]) for i in range(len_)]
     return images
